654cb035fd038be1837237df9d3c88dbbbb599b0
wildlifehexagon <crossthebreeze@gmail.com>
2019-01-10 17:19:50 -0600

    refactor: update messaging to use new interface and structs

--- internal/message/message.go
+++ internal/message/message.go
@@ -4,22 +4,16 @@ import (
 	"github.com/dictyBase/go-genproto/dictybaseapis/order"
 )
 
-// Subscriber is a generic interface to encapsulate how we want our subscribers
-// to behave. For now the system will auto stop if it encounters any errors. If
-// a user encounters a closed channel, they should check the Err() method to see
-// what happened.
-type Subscriber interface {
-	// Start will return a channel of raw messages.
-	Start(string, *order.Order) <-chan SubscriberMessage
-	// Err will contain any errors returned from the consumer connection.
-	Err() error
-	// Stop will initiate a graceful shutdown of the subscriber connection.
-	Stop() error
+// IssueTracker is the interface for methods related to the Github Issue Tracker.
+type IssueTracker interface {
+	// CreateIssue creates a new GitHub issue.
+	CreateIssue() (*order.Order, error)
 }
 
-// SubscriberMessage is a struct to encapsulate subscriber messages and provide
-// a mechanism for acknowledging messages _after_ they've been processed.
-type SubscriberMessage interface {
-	Message() []byte
-	Done() error
+// NatsSubscriber is an interface to encapsulate the behavior of subscribers.
+type NatsSubscriber interface {
+	// Start will begin subscription for handling new stock orders.
+	Start(string, *order.Order) error
+	// Stop will initiate a graceful shutdown of the subscriber connection.
+	Stop() error
 }
--- internal/message/nats/sub.go
+++ internal/message/nats/sub.go
@@ -1,76 +1,53 @@
 package nats
 
 import (
+	"fmt"
+
 	"github.com/dictyBase/event-messenger/internal/message"
 
 	"github.com/dictyBase/go-genproto/dictybaseapis/order"
 
 	gnats "github.com/nats-io/go-nats"
+	"github.com/nats-io/go-nats/encoders/protobuf"
 )
 
-type natsMessage struct {
-	msg *gnats.Msg
+type natsSubscriber struct {
+	econn *gnats.EncodedConn
 }
 
-func (m *natsMessage) Message() []byte {
-	return m.msg.Data
+// NewSubscriber connects to nats
+func NewSubscriber(host, port string, options ...gnats.Option) (message.NatsSubscriber, error) {
+	nc, err := gnats.Connect(fmt.Sprintf("nats://%s:%s", host, port), options...)
+	if err != nil {
+		return &natsSubscriber{}, err
+	}
+	ec, err := gnats.NewEncodedConn(nc, protobuf.PROTOBUF_ENCODER)
+	if err != nil {
+		return &natsSubscriber{}, err
+	}
+	return &natsSubscriber{econn: ec}, nil
 }
 
-func (m *natsMessage) Done() error {
-	if err := m.msg.Sub.Unsubscribe(); err != nil {
+// Start starts the server and communicates using a channel
+func (n *natsSubscriber) Start(sub string, ord *order.Order) error {
+	_, err := n.econn.Subscribe(sub, func(sub string, ord *order.Order) {
+		// this is where the issue needs to be created
+		// need get the obj from protocol buffer and then use that data
+	})
+	if err != nil {
+		return err
+	}
+	if err := n.econn.Flush(); err != nil {
+		return err
+	}
+	if err := n.econn.LastError(); err != nil {
 		return err
 	}
 	return nil
 }
 
-type natsSubscriber struct {
-	conn   *gnats.Conn
-	err    error
-	output chan message.SubscriberMessage
-}
-
-// NewSubscriber connects to nats
-func NewSubscriber(url string, options ...gnats.Option) (message.Subscriber, error) {
-	nc, err := gnats.Connect(url, options...)
-	if err != nil {
-		return &natsSubscriber{}, err
-	}
-	return &natsSubscriber{
-		conn:   nc,
-		output: make(chan message.SubscriberMessage),
-	}, err
-}
-
-// Start starts the server and communicates using a channel
-func (s *natsSubscriber) Start(sub string, ord *order.Order) <-chan message.SubscriberMessage {
-	nm := &natsMessage{}
-	_, err := s.conn.Subscribe(sub, func(msg *gnats.Msg) {
-		// this is where the issue needs to be created
-		// need get the obj from protocol buffer and then use that data
-		nm.msg = msg
-		s.output <- nm
-	})
-	if err != nil {
-		s.err = err
-		return s.output
-	}
-	if err := s.conn.Flush(); err != nil {
-		s.err = err
-		return s.output
-	}
-	if err := s.conn.LastError(); err != nil {
-		s.err = err
-		return s.output
-	}
-	return s.output
-}
-
-func (s *natsSubscriber) Err() error {
-	return s.err
-}
-
-func (s *natsSubscriber) Stop() error {
-	s.conn.Close()
-	close(s.output)
+// Stop stops the server
+func (n *natsSubscriber) Stop() error {
+	n.econn.Close()
 	return nil
 }
