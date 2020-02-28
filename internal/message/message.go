787fccf3369bd599cdb43b764c150e710b62abac
Siddhartha Basu <biosidd@gmail.com>
2020-02-28 17:33:00 -0600

    refactor: Shutdown interface now contain the stop method

--- internal/message/message.go
+++ internal/message/message.go
@@ -5,18 +5,20 @@ import (
 	email "github.com/dictyBase/event-messenger/internal/send-email"
 )
 
+// Shutdown interface is for handling stopping of connection
+type Shutdown interface {
+	// Stop will initiate a graceful shutdown of the subscriber connection.
+	Stop() error
+}
+
 // GithubSubscriber is an interface to encapsulate the behavior of Github subscribers.
 type GithubSubscriber interface {
 	// Start will begin the subscription for creating Github issues for new stock orders.
 	Start(string, issue.IssueTracker) error
-	// Stop will initiate a graceful shutdown of the subscriber connection.
-	Stop() error
 }
 
 // GmailSubscriber is an interface to encapsulate the behavior of Gmail subscribers.
 type GmailSubscriber interface {
 	// Start will begin the subscription for sending an email when a new stock order is received.
 	Start(string, email.EmailHandler) error
-	// Stop will initiate a graceful shutdown of the subscriber connection.
-	Stop() error
 }
