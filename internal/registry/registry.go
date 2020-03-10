52a3625e0724458f38df40d020d4ad3a6390599d
Siddhartha Basu <biosidd@gmail.com>
2020-03-10 13:09:50 -0500

    feat: separate package to manage data from various services

---
+++ internal/registry/registry.go
@@ -0,0 +1,17 @@
+package registry
+
+const (
+	StrainInvOnto     = "strain_inventory"
+	PlasmidInvOntO    = "plasmid_inventory"
+	InvLocationTag    = "location"
+	InvStoredAsTag    = "stored as"
+	InvVialCountTag   = "number of vials"
+	InvVialColorTag   = "color"
+	PlasmidInvTag     = "plasmid inventory"
+	StrainInvTag      = "strain_inventory"
+	SysnameTag        = "systematic name"
+	DictyAnnoOntology = "dicty_annotation"
+	SynTag            = "synonym"
+	StrainPrice       = 30
+	Plasmid           = 15
+)
