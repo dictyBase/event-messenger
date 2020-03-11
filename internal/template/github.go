0514042d53e063bfcd16139d195606ad99822241
Siddhartha Basu <biosidd@gmail.com>
2020-03-10 20:08:11 -0500

    refactor: add a separate package for inline template

---
+++ internal/template/github.go
@@ -0,0 +1,59 @@
+package template
+
+const IssueTmpl = `
+ Shipping and billing information   
+
+|	Shipping address									 |		  | Billing address	    						 |
+: -------------------------------------------------------|--------|----------------------------------------------:
+|   {{- with .Shipper.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Courier}} {{.CourierAccount}} {{- end }} |          | {{- with .Shipper.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Courier}} {{.CourierAccount}} {{- end }} |          | {{- with .Shipper.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Courier}} {{.CourierAccount}} {{- end }} |          | {{- with .Shipper.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Courier}} {{.CourierAccount}} {{- end }} |   | {{- with .Payer.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Courier}} {{.CourierAccount}} {{- end }} |          | {{- with .Shipper.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Courier}} {{.CourierAccount}} {{- end }} |          | {{- with .Shipper.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Courier}} {{.CourierAccount}} {{- end }} |   | {{- with .Shipper.Data.Attributes }} {{.FirstName}} {{.LastaName}} <br/> {{.Organization}}	<br/> {{.FirstAddress}} <br/> {{.SecondAddress}} <br/> {{.City}} {{.State}} {{.ZipCode}} <br/> {{.Country}} <br/> Phone: {{.Phone}} <br/> {{.Email}} <br/> {{.Payment}} {{- end }} |
+
+{{if .Strains}}
+# Stocks ordered
+
+|	Item	|	Quantity 	     |	Unit price($)	  |	Total($)	       |
+|-----------|--------------------|--------------------|--------------------|
+|	Strain	| {{.StrainItems}}   | {{.StrainPrice}}   |  {{.StrainCost}}   |
+|	Plasmid	| {{.PlasmidItems}}  | {{.PlasmidPrice}}  |  {{.PlasmidCost}}  |
+|			|					 |					  |	 {{.TotalCost}}    |
+
+# Strain information 
+
+|  ID							  |  Descriptor					   |	Name(s)		               |	Systematic Name				   |	Characteristics			    | |---------------------------------|--------------------------------|-------------------------------|-----------------------------------|--------------------------------|
+{{- range $idx,$e := .StrainInfo}}
+| {{index $e 0}}    | {{index $e 1}}  | {{index $e 2}} |  {{index $e 3}}    |  {{index $e 4}} |
+{{- end}}
+	
+
+# Strain storage
+{{if .StrainInv}}
+
+|	Name						 |	Stored as					 |	Location					|	No. of vials				|	Color					   |
+|--------------------------------|-------------------------------|------------------------------|-------------------------------|------------------------------|
+{{- range $idx,$e := .StrainInv}}
+| {{index $e 0}}    | {{index $e 1}}  | {{index $e 2}} |  {{index $e 3}}    |  {{index $e 4}} |
+{{- end}}
+{{else}}
+## No strain inventories, POP,CRAKLE,BOOM!!!!!
+{{end}}
+
+{{end}}
+
+
+{{if and .Plasmids .PlasmidInv}}
+# Plasmid information and storage
+
+|	ID				   |	Name		     |	Stored as	     |	Location         |	Color	     |
+|----------------------|---------------------|-------------------|-------------------|---------------|
+{{- range $idx,$e := .PlasmidInv}}
+| {{index $e 0}}    | {{index $e 1}}  | {{index $e 2}} |  {{index $e 3}}    |  {{index $e 4}} |
+{{- end}}
+{{else}}
+# No plasmid inventories no order
+{{end}}
+
+{{if .Order.Data.Attributes.Comments}}
+# Comment
+{{.Order.Data.Attributes.Comments}}
+{{end}}
+
+`
