ea09464407a814a8943a9128a5269ea94d0e9a02
Siddhartha Basu <biosidd@gmail.com>
2020-03-19 10:42:28 -0500

    refactor: move the issue template to the assets foler

--- internal/template/issue.go
+++ internal/template/issue.go
@@ -1,1 +1,12 @@
 package template
+
+import "github.com/dictyBase/go-genproto/dictybaseapis/stock"
+
+type IssueContent struct {
+	Strains    []*stock.Strain
+	Plasmids   []*stock.Plasmid
+	StrainInv  [][]string
+	PlasmidInv [][]string
+	StrainInfo [][]string
+	*Content
+}
