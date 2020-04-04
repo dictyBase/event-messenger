84896e5a31b0126b9ad8062eb2389a76639a4b08
Siddhartha Basu <biosidd@gmail.com>
2020-04-03 20:34:10 -0500

    refactor: remove extra attributes that are no longer needed for markdown template

--- internal/template/issue.go
+++ internal/template/issue.go
@@ -1,10 +1,6 @@
 package template
 
-import "github.com/dictyBase/go-genproto/dictybaseapis/stock"
-
 type IssueContent struct {
-	Strains    []*stock.Strain
-	Plasmids   []*stock.Plasmid
 	StrainInv  [][]string
 	PlasmidInv [][]string
 	StrainInfo [][]string
