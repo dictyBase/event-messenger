9f10bdac9e388dfdf50ce312ed19f31305ec959c
Siddhartha Basu <biosidd@gmail.com>
2020-04-20 12:42:26 -0500

    test: tests for shippping and payers address

--- internal/template/issue_test.go
+++ internal/template/issue_test.go
@@ -2,6 +2,7 @@ package template
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
+	mr "github.com/yuin/goldmark/renderer/html"
 )
 
 func TestIssuePlasmidMkdown(t *testing.T) {
@@ -21,12 +23,16 @@ func TestIssuePlasmidMkdown(t *testing.T) {
 	})
 	assert.NoError(err, "expect no error from rending issue content")
 	var out bytes.Buffer
-	md := goldmark.New(goldmark.WithExtensions(extension.GFM))
+	md := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithRendererOptions(mr.WithUnsafe()),
+	)
 	err = md.Convert(b.Bytes(), &out)
 	assert.NoError(err, "expect no error from markdown conversion")
 	doc, err := goquery.NewDocumentFromReader(&out)
 	assert.NoError(err, "expect no error from reading html output")
 	testMarkdownOrderHeader(t, doc, ic)
+	testMarkdownOrderAddress(t, doc, ic)
 }
 
 func testMarkdownOrderHeader(t *testing.T, doc *goquery.Document, ic *IssueContent) {
@@ -45,5 +51,74 @@ func testMarkdownOrderHeader(t *testing.T, doc *goquery.Document, ic *IssueConte
 		),
 		"should match order id",
 	)
+}
 
+func testMarkdownOrderAddress(t *testing.T, doc *goquery.Document, ic *IssueContent) {
+	assert := assert.New(t)
+	assert.Exactly(
+		"Shipping address",
+		doc.Find("table>thead").Eq(0).Find("tr>th:first-child").Text(),
+		"should match shipping header",
+	)
+	assert.Exactly(
+		"Billing address",
+		doc.Find("table>thead").Eq(0).Find("tr>th:nth-child(3)").Text(),
+		"should match billing header",
+	)
+	assert.Exactly(
+		"Harrold Pennypacker",
+		strings.TrimSpace(
+			doc.Find("table>tbody").Eq(0).
+				Find("tr>td:nth-child(1)").Contents().
+				Eq(0).Text(),
+		),
+		"expect to matcher the consumers name",
+	)
+	assert.Exactly(
+		"pennpacker@dictycr.org",
+		strings.TrimSpace(
+			doc.Find("table>tbody").Eq(0).
+				Find("tr>td:nth-child(1)>a").Text(),
+		),
+		"expect to matcher the consumers email",
+	)
+	assert.Exactly(
+		"Kel Varnsen",
+		strings.TrimSpace(
+			doc.Find("table>tbody").Eq(0).
+				Find("tr>td:nth-child(3)").Contents().
+				Eq(0).Text(),
+		),
+		"expect to matcher the payers name",
+	)
+	assert.Exactly(
+		"varnsen@dictycr.org",
+		strings.TrimSpace(
+			doc.Find("table>tbody").Eq(0).
+				Find("tr>td:nth-child(3)>a").Text(),
+		),
+		"expect to matcher the payers email",
+	)
+	assert.Exactly(
+		strings.TrimPrefix(
+			doc.Find("table>tbody").Eq(0).
+				Find("tr>td:nth-child(1)").Contents().
+				Last().Text(),
+			" "),
+		fmt.Sprintf(
+			"%s %s",
+			ic.Order.Data.Attributes.Courier,
+			ic.Order.Data.Attributes.CourierAccount,
+		),
+		"should match courier information",
+	)
+	assert.Exactly(
+		strings.TrimPrefix(
+			doc.Find("table>tbody").Eq(0).
+				Find("tr>td:nth-child(3)").Contents().
+				Last().Text(),
+			" "),
+		ic.Order.Data.Attributes.Payment,
+		"should match payment information",
+	)
 }
