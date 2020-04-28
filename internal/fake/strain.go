06d79a3532998a6d5ecbb091d4cb761cfe1c7312
Siddhartha Basu <biosidd@gmail.com>
2020-04-28 11:56:19 -0500

    feat: add new package for providing fake data

---
+++ internal/fake/strain.go
@@ -0,0 +1,18 @@
+package fake
+
+func StrainItems() []string {
+	return []string{
+		"DBS0236414",
+		"DBS0236440",
+		"DBS0236245",
+		"DBS0235559",
+	}
+}
+
+func PlasmidItems() []string {
+	return []string{
+		"DBP0000105",
+		"DBP0000034",
+		"DBP0000120",
+	}
+}
