c5815421af060639133a8a466b2129788facb785
Siddhartha Basu <biosidd@gmail.com>
2020-04-28 19:16:25 -0500

    test: add unit test for GetStrainInv method

--- internal/datasource/annotation_test.go
+++ internal/datasource/annotation_test.go
@@ -23,6 +23,26 @@ func mockedAnnoClient() *TaggedAnnotationServiceClient {
 	return mockedAnnoClient
 }
 
+func TestGetStrainInv(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	stock := &Stock{Client: mockedStockClient()}
+	strains, err := stock.GetStrains(fake.StrainIds())
+	assert.NoError(err, "expect no error from getting strains")
+	ann := &Annotation{Client: mockedAnnoClient()}
+	invList, err := ann.GetStrainInv(strains)
+	assert.NoError(err, "expect no error from getting strains")
+	assert.Len(invList, 16, "should match no of groups in collection")
+	for _, inv := range invList {
+		assert.Len(inv, 5, "should have 5 entries for each inventory")
+		assert.Exactly(inv[0], "yS13", "should match the strain lab4el")
+		assert.Exactly(inv[1], "axenic cells", "should match how strain is stored")
+		assert.Exactly(inv[2], "2-9(55-57)", "should match storage location of strain")
+		assert.Exactly(inv[3], "9", "should match no of vials")
+		assert.Exactly(inv[4], "blue", "should match the color of storage vial")
+	}
+}
+
 func TestGetsysName(t *testing.T) {
 	t.Parallel()
 	assert := assert.New(t)
